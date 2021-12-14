68cb7e54f893834081c438b40e892d3f5402c68d
daijiaojiao <2439877294@qq.com>
2021-12-14 15:48:07 +0800

    feat; qiankun

--- src/main.js
+++ src/main.js
@@ -1,5 +1,42 @@
+// import { createApp } from "vue";
+// import App from "./App.vue";
+// import router from "./router";
+
+// createApp(App).use(router).mount("#app");
+if (window.__POWERED_BY_QIANKUN__) {
+  // eslint-disable-next-line no-undef
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+}
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+let instance = null;
 
-createApp(App).use(router).mount("#app");
+function render(props = {}) {
+  const { container } = props;
+
+  instance = createApp(App);
+  instance.use(router);
+  instance.mount(container ? container.querySelector("#app") : "#app");
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log("%c ", "color: green;", "vue3.0 app bootstraped");
+}
+
+export async function mount(props) {
+  render(props);
+  instance.config.globalProperties.$onGlobalStateChange =
+    props.onGlobalStateChange;
+  instance.config.globalProperties.$setGlobalState = props.setGlobalState;
+}
+
+export async function unmount() {
+  instance.unmount();
+  instance._container.innerHTML = "";
+  instance = null;
+}
--- src/router/index.js
+++ src/router/index.js
@@ -3,23 +3,20 @@ import Home from "../views/Home.vue";
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "Home",
     component: Home,
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(window.__POWERED_BY_QIANKUN__ ? "/app2" : "/"),
   routes,
 });
 
